a04c44005b2b74621ffe59df9c63ff89174c42a9
Дмитрий Берестнев <dberestnev@rteam.tech>
2023-09-22 18:33:13 +0300

    fix

--- app_2.py
+++ app_2.py
@@ -1,3 +1,3 @@
 first_number = int(input("Введите первое число:"))
 second_number = int(input("Введите второе число:"))
-print((1 << first_number) + (1 << second_number))
+print((1 << first_number) | (1 << second_number))
