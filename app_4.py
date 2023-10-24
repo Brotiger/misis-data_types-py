c61ac0b80abb4aed6b4e29c2963126f96dee5cc8
Дмитрий Берестнев <dberestnev@rteam.tech>
2023-10-24 17:13:56 +0300

    fix app 4

--- app_4.py
+++ app_4.py
@@ -1,4 +1,11 @@
 input_number = int(input("Введите число:"))
-first_half = (input_number // 100000) % 10 * 100 + (input_number // 10000) % 10 * 10 + (input_number  // 1000) % 10
-second_half = (input_number // 100) % 10 + (input_number // 10) % 10 * 10 + input_number % 10 * 100
-print(int(first_half == second_half))
+
+x1 = (input_number // 100000) % 10
+x2 = (input_number // 10000) % 10
+x3 = (input_number // 1000) % 10
+
+y1 = input_number % 10
+y2 = (input_number // 10) % 10
+y3 = (input_number // 100) % 10
+
+print(int(((x1 & y1) / x1) and ((x2 & y2) / x2) and ((x3 & y3) / x3)))
